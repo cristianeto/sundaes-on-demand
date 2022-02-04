2ed543c331b5a3ad630a59c5c2ec4bd620793d66
Cristian Guaman <cristian.guaman@icloud.com>
2022-02-04 13:52:40 -0500

    [feat,test] Code and test scopp options images from server

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,8 +1,9 @@
-import { rest } from 'mws';
+import { rest } from 'msw';
 
-export const handlers = () => [
+export const handlers = [
   rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
     return res(
+      ctx.status(200),
       ctx.json([
         { name: 'Chocolate', imagePath: '/images/chocolate.png' },
         { name: 'Vanilla', imagePath: '/images/vanilla.png' },
