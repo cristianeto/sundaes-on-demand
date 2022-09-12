7c327c3a0a91764c0305cdd2829f4582eb8d5380
Cristian Guaman <cristian.guaman@icloud.com>
2022-09-12 13:33:17 -0500

    test: Builded Order Sumary

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import './App.css';
-import { SummaryForm } from './pages/summary/SummaryForm';
+import { Container } from 'react-bootstrap';
+import { OrderDeatilsProvider } from './contexts/OrderDetails';
+import OrderEntry from './pages/entry/OrderEntry';
 
 function App() {
-  return <SummaryForm />;
+  return (
+    <Container>
+      <OrderDeatilsProvider>
+        {/** Sumary page and entry page need provider */}
+        <OrderEntry />
+      </OrderDeatilsProvider>
+      {/**Confirmation page does not need a provider*/}
+    </Container>
+  );
 }
 
 export default App;
